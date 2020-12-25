9d8c1f2f950ea9b4ca35856a5453fd9b8fef30a3
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-25 20:19:57 -0300

    Create uri 1002.js

---
+++ uri 1002.js
@@ -0,0 +1,9 @@
+const input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+var raio = parseFloat(lines.shift());
+var PI = 3.14159;
+var area = PI * Math.pow(raio, 2);
+
+
+console.log ("A=" + area.toFixed(4));
