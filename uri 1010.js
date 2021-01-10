d63aeaec62dc68816954194f2969c3345b465aa9
Gabriel Carneiro <gabriel29music@gmail.com>
2021-01-09 21:10:09 -0300

    Create uri 1010.js

---
+++ uri 1010.js
@@ -0,0 +1,24 @@
+const input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+let [idpeca1, quantpeca1, valorpeca1] = lines.shift().split(' ');
+let [idpeca2, quantpeca2, valorpeca2]= lines.shift().split(' ');
+
+
+let total1 = valorpeca1 * quantpeca1;
+
+
+let total2 = valorpeca2 * quantpeca2;
+
+
+
+let total = total1 + total2;
+console.log(`VALOR A PAGAR: R$ ${total.toFixed(2)}`);
+
+/*Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+
+Saída
+A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.*/
